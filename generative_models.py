d5b8db7262b1ed937a7203972550c617c2dec8e6
Lee Sharkey <leedsharkey@gmail.com>
2021-03-12 18:04:57 +0100

    Added most of the layers we'll need and updated thinking on generative model

--- generative_models.py
+++ generative_models.py
@@ -1,33 +1,56 @@
 import torch
 import torch.nn as nn
 from policies import ResidualBlock
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import numpy as np
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.nn import init
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.nn import Parameter as P
 
-"""The generative model consists of 
+from sync_batchnorm import SynchronizedBatchNorm2d as SyncBN2d
+
+"""Proposal for generative model:
+
     - An encoder network
-        - image -> residual (128)
-        - init hidden state added to the channel dim (256)
-        - both into 1x1 conv (-> 128)
-        - residual block 256
+        - image -> residual (64)
+        - init hidden state added to the channel dim (64+128)
+        - all (image, init hidden, and resid output) into 1x1 conv (3+64+128 -> 128)
         - layer norm
-        - residual shrink block 256
+        - Resblock down
+        - non-local layer
         - layer norm
-        - residual shrink block (or large kerns to ensure mixing) 256
+        - Resblock down 256 (with dense inputs from above)
+        - layer norm
+        - Resblock down (256)
         - layer norm
         - split
             - fc to mu (should have around 256 neurons)
             - fc to sigma
     - A decoder network
-        - inithidden&andActionNetwork
-            - residual block
-            - layer norm
-            - split
-                - a)  
-                    - residual shrink block
-                    - FC to init hidden state
-                - b)
-                    - residual shrink block
-                    - FC to prev action
-        - standardisernet
+        - Initializer networks
+            - inithidden&andActionNetwork
+                - residual block
+                - layer norm
+                - split
+                    - a)  
+                        - residual shrink block
+                        - FC to init hidden state
+                    - b)
+                        - residual shrink block
+                        - FC to prev action
+            - ConvGRU initializer
+                - This takes a guess at initializing the GRU so 
+                  it doesn't start with 0-tensors. Informative initialization 
+                  like this should work better.            
+            
+        - StandardizerNet
             - fc to large block (4x4x256)
             - layernorm
             - deconv growth residual block 
@@ -37,11 +60,9 @@ from policies import ResidualBlock
             - conv
             - layer norm
             now at standard block, which should be roughly 8x8x128
-        - UnrollerNet
-            - AssimilatorResidualBlock
-            - layer norm
-            - ResidualConv
-            - layer norm
+        - UnrollerNet (needs nonlocal)
+            - AssimilatorResidualBlock (takes standard block and also noise vector and GRU hidden state and outputs standard block) 
+            - ConvGRU
         - Side decoders (take a standard block and produce predictions for obs and rew
             -reward decoder
                 - Residual shrink block
@@ -56,41 +77,44 @@ from policies import ResidualBlock
                 - layer norm
                 - conv (but reduce channels) (3)
             
-            
+            -  AssimilatorResidualBlock
+                - has-a:
+                    - AssimilatorBlock (1x1 conv to 2d conv)
+                    - residual connection between non vec inputs to AssimilatorBlock and its outputs
+"""
 
+"""We'll use a convGRU unroller and have a non local layer in the obs decoder
+At the input to the convGRU at every timestep we'll have an assimilator
+that takes the noise, the action, and the hidden state and projects it
+into something that's the same size as the hidden state, to which we'll add it.
+This way, the assimilator is a res net and just updates the inputs to the
+recurrent network.  
 """
 
 """
 Classes we'll need:
-DeconvResidualBlock
-Deconv Growth Residual Block (a growth deconv in front of DeconvResidual)
+- ResidualBlock for upsample and downsample and constant (from BigGAN)
+- non-local layer (Attention)
 - AssimilatorResidualBlock
+- AssimilatorBlock
+- Encoder
+- Decoder
+    - InitializerNets
+    - Standardizer
+    - UnrollerNet
+    - reward decoder
+    - obs decoder
 """
 
+"""
+Later, if that isn't producing good results, we can try an unroller that's
+just a resblock. The reason i didn't go for this for the main attempt is that
+I don't see why we wouldn't get vanishing or exploding gradients, and I 
+couldn't see any literature that did this. We can try something like:
+- UnrollerNet
+    - AssimilatorResidualBlock (takes standard block and also noise vector and outputs standard block) 
+    - layer norm
+    - ResidualConv
+    - layer norm
+"""
 
-class AssimilatorResidualBlock(nn.Module):
-
-    def __init__(self, channels,
-                 actv=torch.relu,
-                 kernel_sizes=[3, 3],
-                 paddings=[1, 1],
-                 action_dim=16):
-        super(ResidualBlock, self).__init__()
-
-        self.conv0 = nn.Conv2d(in_channels=channels+action_dim,
-                               out_channels=channels,
-                               kernel_size=kernel_sizes[0],
-                               padding=paddings[0])
-        self.conv1 = nn.Conv2d(in_channels=channels+action_dim,
-                               out_channels=channels,
-                               kernel_size=kernel_sizes[1],
-                               padding=paddings[0])
-        self.actv = actv
-
-    def forward(self, x):
-        inputs = x
-        x = self.actv(x)
-        x = self.conv0(x)
-        x = self.actv(x)
-        x = self.conv1(x)
-        return x + inputs
