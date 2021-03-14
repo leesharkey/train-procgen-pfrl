dedd80a01cde42d2b90e494d6bf669d68e216228
Daniel <daniel@braun.com.au>
2021-03-14 14:03:10 +1100

    Handle multiple obs per input in fake data

--- train_generative.py
+++ train_generative.py
@@ -115,13 +115,17 @@ def train(epoch, configs, train_loader, gen_model, agent, logger, log_dir, devic
             logger.info('Model save to {}'.format(model_path))
 
 
-def get_fake_data(num_obs, obs_size, act_space_size, batch_size):
+def get_fake_data(num_obs, obs_per_input, obs_size, act_space_size, batch_size):
     """
     Notes from proposal doc:
     Data:
         K-sized minibatch where each element contains:
-            (J-1*observations from timestep T-J to T-1) and
-            initial recurrent states at timestep T-J
+            (J*observations from timestep T-J to T-1) and
+            initial recurrent states at timestep T-J, where:
+                J = the number of observations that we're feeding into the VAE
+                T = the 0th timestep that we're actually predicting (i.e. the first timestep in the future)
+                So we're feeding observations from timestep (T-J):(T-1) (inclusive) into the VAE
+                And we also want the VAE to produce initial hidden states at timestep T-J
             Action log probabilities (vector)
             Action (integer)
             Agent value function output (scalar)
@@ -138,24 +142,26 @@ def get_fake_data(num_obs, obs_size, act_space_size, batch_size):
             k*(L*observations from timestep T to T+L))
     ############################################################
     Args:
-        num_obs: total number of observations in dataset
-        obs_size: size of each observation (I think for coinrun we will use 64x64x3?)
+        num_obs: total number of observations in the dataset
+        obs_per_input: the number of observations in each VAE input (denoted by J in the docstring)
+        obs_size: (tuple) - size of each observation (I think for coinrun we will use 64x64x3?)
         act_space_size: number of actions available (15 for coinrun?)
-        batch_size: size of each batch
+        batch_size: the number of VAE inputs in each batch
     Returns:
         batches of data of size batch_size
     """
-    num_batches = num_obs // batch_size
+    num_batches = num_obs // obs_per_input // batch_size
     data = []
     for batch in range(num_batches):
-        actions, action_vals, reward, timestep, episode, done, lvl_seed = (
-            np.random.randn(batch_size), np.random.randn(batch_size), np.random.randn(batch_size),
-            np.random.randn(batch_size), np.random.randn(batch_size), np.random.randn(batch_size),
-            np.random.randn(batch_size),
-        )
-        obs = np.array([np.random.rand(*obs_size) for _ in range(batch_size)])
-        rec_state = np.array([np.random.rand(*obs_size) for _ in range(batch_size)])
-        action_log_probs = np.array([np.random.rand(act_space_size) for _ in range(batch_size)])
+        actions, action_vals, reward, timestep, episode, done, lvl_seed = [
+            np.random.randn(batch_size*obs_per_input) for _ in range(7)
+        ]
+        obs = [np.random.rand(*obs_size) for _ in range(batch_size*obs_per_input)]
+        rec_state = [np.random.rand(*obs_size) for _ in range(batch_size*obs_per_input)]
+        # This assumes that we have 1 action for every observation in our data. Not sure if it is standard to take
+        # recordings finishing at the state or action.
+        action_log_probs = [np.random.rand(act_space_size) for _ in range(batch_size*obs_per_input)]
+
         # Since enumerating trainloader returns batch_idx, (data,_), we make this a tuple.
         data.append(({
             'actions': actions, 'action_vals': action_vals, 'reward': reward, 'timestep': timestep,
@@ -209,7 +215,7 @@ def run():
 
     # Set up dataset
     train_loader = None
-    train_loader = get_fake_data(100, (64,64,3), 2, 10)
+    train_loader = get_fake_data(1000, 5, (64,64,3), 15, 20)
     # train_loader = torch.utils.data.DataLoader(
     #     datasets.MNIST('../data', train=True, download=True,
     #                    transform=transforms.ToTensor()),
@@ -233,4 +239,4 @@ def run():
 
 if __name__ == "__main__":
     run()
-    # train_loader = get_fake_data(10, (2,2,1), 2, 2)
+    # train_loader = get_fake_data(12, 3, (2,2,1), 2, 2)
